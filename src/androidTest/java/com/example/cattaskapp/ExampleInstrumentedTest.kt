db23674acfae008c93b41f78f6636191e69bc6e0
Bahdan Dzioba <dziobadzz@gmail.com>
2021-09-28 21:42:15 +0300

    fix: pull test

--- src/androidTest/java/com/example/cattaskapp/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com/example/cattaskapp/ExampleInstrumentedTest.kt
@@ -1,20 +1,23 @@
 package com.example.cattaskapp
 
-import androidx.activity.viewModels
-import androidx.lifecycle.LiveData
+
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.cattaskapp.apidata.Cat
 import com.example.cattaskapp.apidata.CatApiImpl
 import com.example.cattaskapp.fragment.State
+import io.mockk.every
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -25,7 +28,6 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.nytimesapp", appContext.packageName)
     }
@@ -40,13 +42,33 @@ class ExampleInstrumentedTest {
     @Test
     fun testListCat(){
 
-        val catViewModel = CatViewModel()
         val a =  MutableLiveData<List<Cat>>()
 
-        catViewModel.viewModelScope.launch {
+        GlobalScope.launch (Dispatchers.Main) {
             a.value = CatApiImpl.getListOfCats()
             assertEquals(10, a.value?.size)
         }
 
     }
+
+    @Test
+    fun testConnectNet() {
+
+        val appContext = InstrumentationRegistry.getInstrumentation().context
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val viewModel = CatViewModel()
+            viewModel.initContext(appContext)
+
+            viewModel?.initContext(appContext)
+            viewModel?.getNewAddItems()
+
+           // Mockito.`when`(viewModel.sorted).thenReturn(viewModel.sorted)
+
+            assertEquals(true, viewModel?.connect?.value)
+
+        }
+
+    }
+
 }
